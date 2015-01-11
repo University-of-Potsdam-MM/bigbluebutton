6c3a01fd942c2bade828367a8d78ea0b2277ba63
emBBB-MF/em <emmkarlapp@gmail.com/em>
2015-01-11 18:31:02 +0100

    changed the way chat and notes are hidden in playback

--- record-and-playback/presentation/playback/presentation/0.9.0/advanced_playback.js
+++ record-and-playback/presentation/playback/presentation/0.9.0/advanced_playback.js
@@ -1,16 +1,36 @@
+var chatAndMedia_height;
+
 function change_chatarea()
 {
 	var chatarea_type = document.getElementsByName("chatarea_type");
+	if (typeof chatAndMedia_height === 'undefined') {
+		chatAndMedia_height = document.getElementById('chat').clientHeight;
+	}
 	if(chatarea_type[0].checked){ //chat
 		chat.style.backgroundColor = "yellow";
-	showChat();
+		chat.style.height = chatAndMedia_height+"px";
+		notes.style.height = "0px";
+		chat.setAttribute('aria-hidden', false);
+		chat.style.display = "block";
+		notes.setAttribute('aria-hidden', true);
+		notes.style.display = "none";
+		showChat();
 	} else if(chatarea_type[1].checked){ //notes
-		chat.style.backgroundColor = "red";
+		notes.style.backgroundColor = "red";
+		notes.style.height = chatAndMedia_height+"px";
+		chat.style.height = "0px";
+		chat.setAttribute('aria-hidden', true);
+		//chat.style.display = "none";
+		notes.setAttribute('aria-hidden', false);
+		notes.style.display = "block";
 		var i = 1;
-	do {
-		changeBlockVisibility("timelineDiv" + i, false);
-		i++;
-	} while (contentDiv!=null);
+		contentDiv = document.getElementById("timelineDiv" + i);
+		while (contentDiv) {
+			contentDiv.setAttribute('aria-hidden', true);
+			contentDiv.style.display = "none";
+			i++;
+			contentDiv = document.getElementById("timelineDiv" + i);
+		} 
 	} else {
 		chat.style.backgroundColor = "blue";
 	}
@@ -31,7 +51,9 @@ function showChat()
 			var j = getLastChatId(timeStamps, pop2.currentTime());
 			var i = 1;
 			while (i <= j) {
-				changeBlockVisibility("timelineDiv" + i, true);
+				contentDiv = document.getElementById("timelineDiv" + i);
+				contentDiv.setAttribute('aria-hidden', false);
+				contentDiv.style.display = "block";
 				i++;
 			}
 		},
@@ -50,14 +72,3 @@ function getLastChatId(noteTimes, currentTime){
 	return j;
 }
 
-function changeBlockVisibility(blockName, visible) {
-	if (visible) {
-		contentDiv = document.getElementById(blockName);
-		contentDiv.setAttribute('aria-hidden', false);
-		contentDiv.style.display = "block";
-	} else {
-		contentDiv = document.getElementById(blockName);
-		contentDiv.setAttribute('aria-hidden', true);
-		contentDiv.style.display = "none";
-	}
-}
